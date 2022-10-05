f6f683886383cb835d2df41040a3df63104b4940
dyedd <1176996982@qq.com>
2022-10-05 17:45:45 +0800

    修复pjax和Mathjax冲突问题

--- component/index.footer.php
+++ component/index.footer.php
@@ -41,6 +41,36 @@
                 lanstar.addComment();
                 lanstar.addPageLike();
                 lanstar.addArchiveToggle();
+            $.getScript("https://cdnjs.loli.net/ajax/libs/mathjax/2.7.7/MathJax.js?config=TeX-AMS-MML_HTMLorMML", function() {
+                MathJax.Hub.Config({
+                    tex2jax: {
+                        inlineMath: [
+                            ['$', '$'],
+                            ['\\(', '\\)']
+                        ]
+                    }
+                });
+                var math = document.getElementsByClassName("article-content")[0];
+                MathJax.Hub.Config({
+                    showProcessingMessages: false, //关闭js加载过程信息
+                    messageStyle: "none", //不显示信息
+                    extensions: ["tex2jax.js"],
+                    jax: ["input/TeX", "output/HTML-CSS"],
+                    tex2jax: {
+                        inlineMath: [
+                            ["$", "$"]
+                        ], //行内公式选择$
+                        displayMath: [
+                            ["$$", "$$"]
+                        ], //段内公式选择$$
+                        skipTags: ['script', 'noscript', 'style', 'textarea', 'pre', 'code', 'a'] //避开某些标签
+                    },
+                    "HTML-CSS": {
+                        availableFonts: ["STIX", "TeX"], //可选字体
+                        showMathMenu: false //关闭右击菜单显示
+                    }
+                });
+                MathJax.Hub.Queue(["Typeset", MathJax.Hub, math]);
                 <?php $this->options->pjax_complete(); ?>
         });
     </script>
