06fb770e7c80d7be360daffa6c1799e340c69970
dyedd <1176996982@qq.com>
2023-01-01 19:06:30 +0800

    fix: 归档样式

--- archives.php
+++ archives.php
@@ -17,53 +17,7 @@ $this->need('layout/header.php');
                 <div class="row">
                     <div class="col-xl-8 col-md-8 col-12 main">
                         <?php $this->need('component/index.toolbar.php'); ?>
-                        <section class="article-info mb-3">
-                            <div class="article-detail">
-                                <h1 class="article-title p-3"><a href="<?php $this->permalink(); ?>">
-                                        <?php $this->title(); ?></a>
-                                </h1>
-                                <p>
-        <span class="article-detail-item">
-            <svg class="icon" aria-hidden="true">
-                <use xlink:href="#icon-shijian"></use>
-            </svg>
-            <time class="create-time" datetime="<?php $this->date('c'); ?>"><?php $this->date(); ?></time>
-        </span>
-                                </p>
-                            </div>
-                            <div class="article-cover-inner">
-                                <img src="<?=  $this->fields->banner ?: utils::indexTheme('assets/img/default.jpg'); ?>" alt="cover">
-                            </div>
-                        </section>
                         <main class="article-main" id="post-<?php $this->cid(); ?>">
-                            <!--面包屑导航-->
-                            <nav aria-label="breadcrumb">
-                                <ol class="breadcrumb pb-2">
-                                    <li class="breadcrumb-item">
-                                        <a href="<?php $this->options->siteUrl(); ?>">
-                                            <svg class="icon" aria-hidden="true">
-                                                <use xlink:href="#icon-shouye1"></use>
-                                            </svg>
-                                            <span>首页</span>
-                                        </a>
-                                    </li>
-                                    <?php if ($this->is('post')): ?>
-                                        <li class="breadcrumb-item active" aria-current="page">
-                                            <svg class="icon" aria-hidden="true">
-                                                <use xlink:href="#icon-fenlei1"></use>
-                                            </svg>
-                                            <?php $this->category(','); ?>
-                                        </li>
-                                    <?php else: ?>
-                                        <li class="breadcrumb-item active" aria-current="page">
-                                            <svg class="icon" aria-hidden="true">
-                                                <use xlink:href="#icon-fenlei1"></use>
-                                            </svg>
-                                            <?php $this->archiveTitle('&raquo;', '', ''); ?>
-                                        </li>
-                                    <?php endif; ?>
-                                </ol>
-                            </nav>
                             <!--文章内容-->
                             <div class="article-content">
                                 <?php $this->widget('Widget_Metas_Tag_Cloud', array('sort' => 'count', 'ignoreZeroCount' => true, 'desc' => true))->to($tags); ?>
