bfe6c0fbdcf6b8828f5db968f2888b69670cdae7
dyedd <1176996982@qq.com>
2023-01-04 17:02:05 +0800

    fix: 优化右侧工具栏

--- layout/footer.php
+++ layout/footer.php
@@ -18,13 +18,15 @@
     <p class="footer-item">Theme By <a href="https://dyedd.cn" title="禁止仿制" class="footer-item">Lanstar</a></p>
 </footer>
 <?php $this->footer(); ?>
-<div class="watch_catalog" onclick="lanstar.addCatalog()">
-    <svg class="icon" aria-hidden="true">
-        <use xlink:href="#icon-mulu"></use>
-    </svg>
-</div>
-<div class="back-to-top" onclick="lanstar.addBackTop()">
-    <svg class="icon" aria-hidden="true">
-        <use xlink:href="#icon-fanhuidingbu-"></use>
-    </svg>
+<div class="tools">
+    <div class="watch_catalog" onclick="lanstar.addCatalog()">
+        <svg class="icon" aria-hidden="true">
+            <use xlink:href="#icon-mulu"></use>
+        </svg>
+    </div>
+    <div class="back-to-top" onclick="lanstar.addBackTop()">
+        <svg class="icon" aria-hidden="true">
+            <use xlink:href="#icon-fanhuidingbu-"></use>
+        </svg>
+    </div>
 </div>
